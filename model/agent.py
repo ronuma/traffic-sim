234d74546486b0c4f6819a499a8296eacd2d7e40
Gabriel Rodriguez <gabordlr@gmail.com>
2023-11-30 22:22:36 -0600

    Commenting the code

--- model/agent.py
+++ model/agent.py
@@ -47,68 +47,68 @@ class Car(Agent):
         """
         Checks if the car can move diagonally
         """
-        if self.dir == " ":
+        if self.dir == " ": # If the car is not moving
             return False
         
-        dict = {"Up": (1, 0), "Down": (-1, 0), "Right": (0, -1), "Left": (0, 1)}
+        dict = {"Up": (1, 0), "Down": (-1, 0), "Right": (0, -1), "Left": (0, 1)} # Dictionary to get the "right" if the car
         
-        all_neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False)
+        all_neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False) # Get all the neighbors of the car
         
-        for neighbor in all_neighbors:
+        for neighbor in all_neighbors: # For each neighbor
             if self.pos[0] + dict[self.dir][0] == neighbor.pos[0] and self.pos[1] + dict[self.dir][1] == neighbor.pos[1]:
-                if isinstance(neighbor, Car) and self.path[-1][0] != self.pos[0] and self.path[-1][1] != self.pos[1]:
+                if isinstance(neighbor, Car) and self.path[-1][0] != self.pos[0] and self.path[-1][1] != self.pos[1]: # If the neighbor is a car and the car is not moving
                     self.patience -= 1
                     return True
         
         else:
             return False
         
-    def move(self):
+    def move(self): 
         """ 
         Determines if the agent can move in the direction that was chosen
         """      
-        if self.path == []:
+        if self.path == []: # If the path is empty
             return 
         
-        neighbors = self.model.grid.get_cell_list_contents(self.path[-1])
+        neighbors = self.model.grid.get_cell_list_contents(self.path[-1]) # Get the neighbors of the car
         
         for agent in neighbors:
-            if isinstance(agent, Car) and self.unique_id != agent.unique_id:
-                self.patience -= 1
-                return
-            elif isinstance(agent, Traffic_Light):
+            if isinstance(agent, Car) and self.unique_id != agent.unique_id: # If the neighbor is a car and the car is not moving
+                self.patience -= 1 # Decrease the patience
+                return #do not move
+            elif isinstance(agent, Traffic_Light): # If the neighbor is a traffic light
                 if not agent.state:                    
-                    return
+                    return #do not move
             # elif (self.check_diagonal()):
             #     return
-            elif isinstance(agent, Destination):
-                self.model.schedule.remove(self)
-                self.model.grid.remove_agent(self)
+            elif isinstance(agent, Destination): # If the neighbor is a destination
+                self.model.schedule.remove(self) # Remove the car from the schedule
+                self.model.grid.remove_agent(self) # Remove the car from the grid
                 self.model.car_count -= 1         
                 self.model.arrived_cars += 1       
                 return
-            elif isinstance(agent, Road):
+            elif isinstance(agent, Road): # If the neighbor is a road
                 self.dir = agent.direction
                  
-        self.model.grid.move_agent(self, self.path[-1])
-        self.path.pop()
+        self.model.grid.move_agent(self, self.path[-1]) # Move the car to the next position
+        self.path.pop() # Remove the last element of the path
         
-    def out_of_patience(self):
-        edges = nx.edges(self.map, [self.pos])        
+    def out_of_patience(self): 
+        edges = nx.edges(self.map, [self.pos]) # Get the edges of the current position of the car
         for edge in edges:         
-            if edge[1] == self.path[-1]:      
-                self.map.edges[edge]['weight'] += 5
-                self.patience = random.randint(5, 10)
+            if edge[1] == self.path[-1]: # If the edge is the same as the next position of the car
+                self.map.edges[edge]['weight'] += 5 # Increase the weight of the edge
+                self.patience = random.randint(5, 10) # Reset the patience
         
-        self.calculate_A_star(self.pos, self.goal)
+        self.calculate_A_star(self.pos, self.goal) # Calculate a new path
         
 
     def step(self):
         """ 
         Determines the new direction it will take, and then moves
         """
-        if self.patience <= 0:
-            self.out_of_patience()
+        if self.patience <= 0: # If the patience is 0
+            self.out_of_patience() # Call the out_of_patience function
         self.move()
 
 class Traffic_Light(Agent):
@@ -125,14 +125,14 @@ class Traffic_Light(Agent):
             state: Whether the traffic light is green or red
             timeToChange: After how many step should the traffic light change color 
         """
-        self.state = state
-        self.timeToChange = timeToChange
+        self.state = state # False = Red, True = Green
+        self.timeToChange = timeToChange # After how many steps should the traffic light change color
 
     def step(self):
         """ 
         To change the state (green or red) of the traffic light in case you consider the time to change of each traffic light.
         """
-        if self.model.schedule.steps % self.timeToChange == 0:
+        if self.model.schedule.steps % self.timeToChange == 0: # If the time to change is reached
             self.state = not self.state
 
 class Destination(Agent):
--- model/model.py
+++ model/model.py
@@ -16,30 +16,31 @@ class CityModel(Model):
     def __init__(self, diagonales = 1.5, paciencia = 1, semaforos = 5):
         # Load the map dictionary. The dictionary maps the characters in the map file to the corresponding agent.
             self.dataDictionary = json.load(open("city_files/mapDictionary.json"))
-            self.map = 0
-            self.destinations = []
-            self.step_count = 0
-            self.car_count = 0
-            self.total_cars = 0
-            self.arrived_cars = 0
+            self.map = 0 # The map of the city
+            self.destinations = [] # The list of destinations
+            self.step_count = 0 # The number of steps that have passed
+            self.car_count = 0 # The number of cars that have been created
+            self.total_cars = 0 # The total number of cars that have been created
+            self.arrived_cars = 0 # The number of cars that have arrived at their destination 
             #variables for sliders
-            self.diagonales = diagonales
-            self.paciencia = paciencia
-            self.semaforos = semaforos
+            self.diagonales = diagonales # parameter for the weight of the diagonals by slider
+            self.paciencia = paciencia # parameter for the patience of the cars by slider
+            self.semaforos = semaforos # parameter for the weight of the traffic lights by slider
             
             self.traffic_lights = []
             graph = nx.DiGraph()  # Change to directed graph     
 
             # Load the map file. The map file is a text file where each character represents an agent.
             with open('city_files/2023_base.txt') as baseFile:
-                lines = baseFile.readlines()
-                self.width = len(lines[0]) - 1
-                self.height = len(lines)
+                lines = baseFile.readlines() # Read the lines of the file
+                self.width = len(lines[0]) - 1 # The width of the map
+                self.height = len(lines) # The height of the map
                 
+                # The spawn points of the cars
                 self.spawn_points = [(0, 0), (self.width - 1, 0), (0,self.height - 1), (self.width - 1, self.height - 1)]
-
-                self.grid = MultiGrid(self.width, self.height, torus=False)
-                self.schedule = RandomActivation(self)
+                
+                self.grid = MultiGrid(self.width, self.height, torus=False) # The grid where the agents are placed
+                self.schedule = RandomActivation(self) # The schedule of the agents
                 
                 self.map = self._init_Graph(graph, lines, self.dataDictionary)
                 
@@ -50,28 +51,28 @@ class CityModel(Model):
                 # Goes through each character in the map file and creates the corresponding agent.
         
     def _init_Graph(self, graph, lines, dataDictionary):
-            for r, row in enumerate(lines):
+            for r, row in enumerate(lines): # r = row, c = column
                 for c, col in enumerate(row):
-                    if col in ["v", "^", ">", "<"]:
-                        agent = Road(f"r_{r * self.width + c}", self, dataDictionary[col])
-                        self.grid.place_agent(agent, (c, self.height - r - 1))
+                    if col in ["v", "^", ">", "<"]: # If the character is a road
+                        agent = Road(f"r_{r * self.width + c}", self, dataDictionary[col]) # Create a new road agent
+                        self.grid.place_agent(agent, (c, self.height - r - 1)) # Place the agent in the grid
                         graph.add_node((c, self.height - r - 1), direction=col)  # Add direction as an attribute
 
-                    elif col in ["S", "s"]:
-                        agent = Traffic_Light(f"tl_{r * self.width + c}", self, False if col == "S" else True, int(dataDictionary[col]))
-                        self.grid.place_agent(agent, (c, self.height - r - 1))
-                        self.schedule.add(agent)
+                    elif col in ["S", "s"]: # If the character is a traffic light
+                        agent = Traffic_Light(f"tl_{r * self.width + c}", self, False if col == "S" else True, int(dataDictionary[col])) # Create a new traffic light agent
+                        self.grid.place_agent(agent, (c, self.height - r - 1))  # Place the agent in the grid
+                        self.schedule.add(agent) # Add the agent to the schedule
                         self.traffic_lights.append(agent)
                         # Add an attribute to mark "S" as "long" and "s" as "short"
                         graph.add_node((c, self.height - r - 1), direction=None, signal_type="long" if col == "S" else "short")
                         # print("signal_type: ", graph.nodes[(c, self.height - r - 1)]['signal_type'])
 
-                    elif col == "#":
-                        agent = Obstacle(f"ob_{r * self.width + c}", self)
+                    elif col == "#": # If the character is an obstacle
+                        agent = Obstacle(f"ob_{r * self.width + c}", self) # Create a new obstacle agent
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
-                    elif col == "D":
-                        agent = Destination(f"d_{r * self.width + c}", self)
+                    elif col == "D": # If the character is a destination
+                        agent = Destination(f"d_{r * self.width + c}", self) # Create a new destination agent
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         graph.add_node((c, self.height - r - 1), direction=None)  # No direction for destination
                         self.destinations.append((c, self.height - r - 1)) # Add destination pos to the list
@@ -81,6 +82,7 @@ class CityModel(Model):
                 
                 if graph.nodes[node]['direction'] == "^":
                     self.adding_edges(graph, node, x, y, 0, 1, [(x - 1, y + 1), (x + 1, y + 1), (x, y + 1), (x - 1 , y), (x + 1, y)], [">", "<"])
+                    #adding_edges(graph, node, x, y, direccionY, direccionY, neighbors a conectar, caracteres especiales)
                 
                 if graph.nodes[node]['direction'] == "v":
                     self.adding_edges(graph, node, x, y, 0, -1, [(x + 1, y - 1), (x - 1, y - 1),(x, y - 1), (x + 1, y), (x - 1, y)], ["<", ">"])
@@ -90,52 +92,51 @@ class CityModel(Model):
                       
                 if graph.nodes[node]['direction'] == ">":
                     self.adding_edges(graph, node, x, y, 1, 0, [(x + 1, y - 1), (x + 1, y + 1), (x + 1, y), (x, y - 1), (x, y + 1)], ["^", "v"])
-                                
-            # self.plot_graph(graph)
-            
-            
-            #Testing agents
-                # agent = Car(f"c_prueba1", self, (1,1), graph, (4,1), True)
-                # agent2 = Car(f"c_prueba2", self, (1,1), graph, (4,1), True)
-                # self.grid.place_agent(agent, (3,0))
-                # self.schedule.add(agent)   
-                # self.grid.place_agent(agent2, (4,1))
-                # self.schedule.add(agent2)    
                 
             return graph     
     
     def adding_edges(self, graph, node, x, y, xVal, yVal, neighbors, chars):
-        for neighbor in neighbors:
-                if neighbor in graph.nodes:
+        for neighbor in neighbors: 
+                if neighbor in graph.nodes: # If the neighbor is in the graph
                     if graph.nodes[neighbor]['direction'] in chars and ( neighbor == neighbors[0] or neighbor == neighbors[1]):
-                        print("")
+                        # If the neighbor is a diagonal and is one of the special characters do not add an edge
+                        print("")                        
                     elif (neighbor == neighbors[3] and graph.nodes[neighbor]['direction'] == chars[1]) or (neighbor == neighbors[4] and graph.nodes[neighbor]['direction'] == chars[0]):
+                        # If the neighbor is a cross and is one of the special characters add an edge
                         graph.add_edge(node, neighbor, weight=1)
-                    elif neighbor == neighbors[2]:                     
+                    elif neighbor == neighbors[2]: 
+                        # If the neighbor is not a diagonal add an edge                                
                         graph.add_edge(node, neighbor, weight=1)
-                    elif neighbor != neighbors[3] and neighbor != neighbors[4]:                     
+                    elif neighbor != neighbors[3] and neighbor != neighbors[4]:
+                        # If the neighbor is a diagonal and is not one of the special characters add an edge
                         graph.add_edge(node, neighbor, weight=self.diagonales)
         
+        # Add edges to the traffic lights depending in which direction they are facing
         if (x + xVal, y + yVal) in graph.nodes and 'signal_type' in graph.nodes[(x + xVal, y + yVal)] and graph.nodes[(x + xVal, y + yVal)]['signal_type'] in ["long", "short"]:
             graph.add_edge((x + xVal, y + yVal), (x + (xVal * 2), y + (yVal * 2)), weight=self.semaforos * 5)
         
         
         
     def add_cars(self):
-        for car in range(len(self.spawn_points)):    
+        isBlocked = True
+        for car in range(len(self.spawn_points)):
                     dest = random.choice(self.destinations) #choose a random destination
-                    new_map = self.map.copy()                    
-                    agent = Car(f"c_{self.total_cars}", self, dest, new_map, self.spawn_points[car], self.paciencia)
+                    new_map = self.map.copy() #copy the map              
+                    agent = Car(f"c_{self.total_cars}", self, dest, new_map, self.spawn_points[car], self.paciencia) #create a new car
                     content = self.grid.get_cell_list_contents(self.spawn_points[car])
-                    if any(isinstance(x, Car) for x in content):  # if the agent is a random agent                        
-                        return
-                    else :
-                        print("Adding agent")
-                        self.grid.place_agent(agent, self.spawn_points[car])
-                        self.schedule.add(agent)      
+                    if any(isinstance(x, Car) for x in content):  # if there is alreade a agent in the cell do not add a new one                          
+                        break
+                    else:
+                        self.grid.place_agent(agent, self.spawn_points[car]) #place the agent in the grid
+                        self.schedule.add(agent)  # Add the agent to the schedule
                         self.car_count += 1 
                         self.total_cars += 1
-
+                        isBlocked = False
+        
+        if isBlocked: #if there is no space for a new car
+            self.running = False #stop the simulation
+        
+        
 
     def plot_graph(self, graph):
         pos = {node: (node[0], -node[1]) for node in graph.nodes}  # Flip y-axis for visualization
@@ -145,7 +146,7 @@ class CityModel(Model):
         plt.show()
 
     def step(self):
-        if self.step_count%3 == 0 and self.step_count != 0:
+        if self.step_count%3 == 0 and self.step_count != 0: #every n steps add a new car
             self.add_cars()
         self.step_count += 1
         self.schedule.step()  
